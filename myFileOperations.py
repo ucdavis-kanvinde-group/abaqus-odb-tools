2817ce169e1b8bf19de90aec29613d71bd45392d
Vincente Pericoli <vspericoli@ucdavis.edu>
2015-09-16 16:14:36 -0700

    can now choose whether to delete or recycle; removed inherent dependency on send2trash module

--- myFileOperations.py
+++ myFileOperations.py
@@ -4,16 +4,13 @@ Vincente Pericoli
 UC Davis
 
 Set of common file operations
-
-Requires the send2trash module
-(see https://pypi.python.org/pypi/Send2Trash)
 """
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 import re
 import sys
+import os
 sys.path.append("C:\\Python27\\Lib\\site-packages")
-from send2trash import send2trash
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 def check_delete(name, verbose=True):
@@ -26,6 +23,37 @@ def check_delete(name, verbose=True):
     """
     if verbose:
         try:
+            os.remove(name)
+            print "\nold file deleted"
+            print "saving new \"%s\"\n" % (name)
+        except:
+            print "\nno file found, saving new file \"%s\"\n" % (name)
+    else:
+        try:
+            os.remove(name)
+        except:
+            pass
+
+def check_recycle(name, verbose=True):
+    """ 
+    places old file in recycle bin, if it exists.
+    this should work for any system, but requires the send2trash module
+    (see https://pypi.python.org/pypi/Send2Trash)
+    
+    input: 
+        name    = file name, with extension (must be in current working dir)
+        verbose = optional input (default=True) which will print messages
+                  to the command window
+    """
+    # import send2trash module:
+    # it's bad practice to import within a function, but the use of
+    # check_recycle is optional, since check_delete exists. Thus, I don't
+    # want the import of myFileOperations to break simply because someone
+    # does not have the optional send2trash module installed.
+    from send2trash import send2trash
+    
+    if verbose:
+        try:
             send2trash(name)
             print "\nold file sent to recycle bin"
             print "saving new \"%s\"\n" % (name)
@@ -36,7 +64,6 @@ def check_delete(name, verbose=True):
             send2trash(name)
         except:
             pass
-
 
 def safe_filename(name):
     """
